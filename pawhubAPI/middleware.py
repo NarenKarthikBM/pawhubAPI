dc82cf9a81f7ca82417ce774147d08e356b548e8
NarenKarthikBM <85060104+NarenKarthikBM@users.noreply.github.com>
2025-08-24 10:03:48 +0530

    cors issue

--- pawhubAPI/middleware.py
+++ pawhubAPI/middleware.py
@@ -50,7 +50,7 @@ class CorsMiddleware:
         # if getattr(settings, "DEBUG", False):
         #     response["Access-Control-Allow-Origin"] = "*"
         # elif origin in allowed_origins:
-        response["Access-Control-Allow-Origin"] = origin
+        response["Access-Control-Allow-Origin"] = "*"
 
         # Set CORS headers
         response["Access-Control-Allow-Methods"] = (
