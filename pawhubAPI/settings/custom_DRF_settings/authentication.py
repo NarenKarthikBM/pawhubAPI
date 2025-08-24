ec252c806f4d5635a5de98b3a0388596dbb0e2b5
NarenKarthikBM <85060104+NarenKarthikBM@users.noreply.github.com>
2025-08-24 09:55:59 +0530

    revert

--- pawhubAPI/settings/custom_DRF_settings/authentication.py
+++ pawhubAPI/settings/custom_DRF_settings/authentication.py
@@ -11,11 +11,13 @@ class UserTokenAuthentication(authentication.BaseAuthentication):
             request.META.get("HTTP_AUTHORIZATION"),
             request.META.get("HTTP_DEVICE_TOKEN"),
         )
-        if not auth_token:
+        if not auth_token or not device_token:
             raise exceptions.AuthenticationFailed("No such user")
 
         try:
-            user_auth_token = UserAuthTokens.objects.get(auth_token=auth_token)
+            user_auth_token = UserAuthTokens.objects.get(
+                auth_token=auth_token, device_token=device_token
+            )
         except UserAuthTokens.DoesNotExist:
             raise exceptions.AuthenticationFailed("No such user")
 
