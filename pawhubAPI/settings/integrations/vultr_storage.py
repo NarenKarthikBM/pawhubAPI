fade9c4d46382dbdac05f4287813217da74fe387
NarenKarthikBM <85060104+NarenKarthikBM@users.noreply.github.com>
2025-08-24 12:52:53 +0530

    vultr conf

--- pawhubAPI/settings/integrations/vultr_storage.py
+++ pawhubAPI/settings/integrations/vultr_storage.py
@@ -6,11 +6,11 @@ from pawhubAPI.settings.env import env
 VULTR_OBJECT_STORAGE_ENABLED = env.bool("VULTR_OBJECT_STORAGE_ENABLED", default=False)
 
 # Vultr Object Storage Credentials
-VULTR_ACCESS_KEY_ID = env.str("VULTR_ACCESS_KEY_ID", default="")
-VULTR_SECRET_ACCESS_KEY = env.str("VULTR_SECRET_ACCESS_KEY", default="")
-VULTR_ENDPOINT_URL = env.str("VULTR_ENDPOINT_URL", default="")
-VULTR_REGION = env.str("VULTR_REGION", default="")
-VULTR_BUCKET_NAME = env.str("VULTR_BUCKET_NAME", default="")
+VULTR_ACCESS_KEY_ID = env.str("VULTR_ACCESS_KEY_ID", default="3XBPP2V081DPDI0KM5MC")
+VULTR_SECRET_ACCESS_KEY = env.str("VULTR_SECRET_ACCESS_KEY", default="yWmDm5mELAI7kfnYDawDEkxh5wJaJfG3z5mAACrW")
+VULTR_ENDPOINT_URL = env.str("VULTR_ENDPOINT_URL", default="https://blr1.vultrobjects.com")
+VULTR_REGION = env.str("VULTR_REGION", default="blr1")
+VULTR_BUCKET_NAME = env.str("VULTR_BUCKET_NAME", default="pawhub-bucket")
 
 # File Upload Settings
 MAX_FILE_SIZE = env.int("MAX_FILE_SIZE", default=10 * 1024 * 1024)  # 10MB default
